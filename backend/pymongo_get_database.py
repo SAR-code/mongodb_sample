88da8badfe872df5907de2477ae463791a99701a
Dylan <dylanmccallum28@gmail.com>
2024-11-26 13:28:45 -0700

    basic backend sample

---
+++ backend/pymongo_get_database.py
@@ -0,0 +1,25 @@
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+def get_database():
+    
+    # Provide the connection string to connect to the database
+    
+    CONNECTION_STRING = os.getenv('DB_STRING')
+    
+    
+    # Establish a connection using MongoClient
+    
+    client = MongoClient(CONNECTION_STRING)
+    
+    # creates a database
+    return client ['user_shopping_list']
+
+# Added for other files to reuse the get_database function
+    
+if __name__ == '__main__':
+        
+    dbname = get_database()
+
+
